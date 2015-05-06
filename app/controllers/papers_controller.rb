58508e61f94e3e3dd8ea4df85b9996840ac207b0
ginpei <ginpei@gmail.com>
2015-05-05 17:25:47 -0700

    use token instead of id for papers

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -66,7 +66,7 @@ class PapersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_paper
-      @paper = Paper.find(params[:id])
+      @paper = Paper.find_by_token(params[:token])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
