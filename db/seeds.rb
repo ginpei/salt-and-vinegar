23e989681262545ae08958174f82a52dee7e0232
ginpei <ginpei@gmail.com>
2015-05-29 18:26:26 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p = Paper.find_by_token('example')
+p.destroy if p
+p = Paper.create({ title: 'Example' })
+p.update(token: 'example')
+p.items.create({ name: 'salt', quantity: '1kg', orderer: 'Alice' })
+p.items.create({ name: 'vinegar' })
